73982e0ebbe49ab0eb1e74bef53b47d3818633a2
bigeatman <dudgp2030@gmail.com>
2022-11-17 11:23:25 +0900

    공감표시 관련 작업중

---
+++ src/main/java/com/my/movie/service/SympathServiceImpl.java
@@ -0,0 +1,24 @@
+package com.my.movie.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.my.movie.dao.SympathDao;
+
+@Repository
+public class SympathServiceImpl implements SympathService{
+
+	@Autowired
+	private SympathDao sympathDao;
+	
+	@Override
+	public Integer isContentLiked(int contentNum, String contentName, int userNum) {
+		return sympathDao.isContentLiked(contentNum, contentName, userNum);
+	}
+
+	@Override
+	public void addContentLiked(int contentNum, String contentName, int userNum, int isLike) {
+		sympathDao.addContentLiked(contentNum, contentName, userNum, isLike);
+	}
+
+}
